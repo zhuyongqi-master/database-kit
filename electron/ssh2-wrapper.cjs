66c5d79a7910ca9fc4b22dd0439ab929c9b2d8b8
zhuyongqi <1023194832@qq.com>
2025-06-03 19:36:31 +0800

    add: Append the missing code file.

--- electron/ssh2-wrapper.cjs
+++ electron/ssh2-wrapper.cjs
@@ -1,9 +1,3 @@
-// CommonJS wrapper for ssh2
 const ssh2 = require('ssh2');
-
-// Export the entire ssh2 module as default export
-// module.exports = ssh2;
 module.exports.default = ssh2;
-
-// Also export individual components that might be needed
 module.exports.Client = ssh2.Client;
